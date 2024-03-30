9e118693bd4c5a01f2b46515393e7b0ca9996061
3Roland59 <amarteyroland359@gmail.com>
2024-03-30 18:01:16 +0000

    d

--- scripts.js
+++ scripts.js
@@ -92,7 +92,10 @@ const createAlarm = (alarmObj) =>{
         })
         alarmDiv.appendChild(checkbox)
         let deleteButton = document.createElement('button')
-        deleteButton.innerHTML = `<i class="fa-solid fa-trash-can">D</i>`
+        deleteButton.innerHTML = `<svg class="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+      </svg>
+      `
         deleteButton.classList.add('deleteButton')
         deleteButton.addEventListener('click', (e)=>(deleteAlarm(e)))
         alarmDiv.appendChild(deleteButton)
